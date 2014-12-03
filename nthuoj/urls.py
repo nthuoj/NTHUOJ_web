0b2af196dcaf24f04554a8f87ef9776e41f33417
Nickel Cheng <nickel.y.j.cheng@gmail.com>
2014-12-03 08:04:14 -0800

    Delete useless system gen urls

--- nthuoj/urls.py
+++ nthuoj/urls.py
@@ -2,9 +2,4 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'nthuoj.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
 )
