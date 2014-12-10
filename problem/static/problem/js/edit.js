efc56763f5be33f6703db2a5dbfa82181fed0601
Gordon <geniusgordon@gmail.com>
2014-12-10 17:59:44 +0800

    merge tag

--- problem/static/problem/js/edit.js
+++ problem/static/problem/js/edit.js
@@ -5,40 +5,67 @@ function switchTab(t) {
     $($(t).attr("href")).addClass("active");
 }
 
-$("a[role='tab']").click(function(e) {
-    e.preventDefault()
-    switchTab(this);
-});
-
 var tags = [];
 
 function escapeHTML(str) {
-	return $('<div/>').text(str).html();
+    return $('<div/>').text(str).html();
 }
 
-function addTag() {
-    var newTag = escapeHTML($('#newTag').val());
-    if (newTag.trim() == "") {
-        alert('empty');
-        return false;
-    }
-    if ($.inArray(newTag.trim().toLowerCase(), tags) != -1) {
-        alert('tag repeated');
-        return false;
-    }
-    document.getElementById('newTag').value = "";
+$(document).ready(function() {
+    $("a[role='tab']").click(function(e) {
+        e.preventDefault()
+        switchTab(this);
+    });
 
-    var click = "onclick=\"return deleteTag('" + newTag + "')\""
-    $('#tagTable').append(
-        "<tr id='" + newTag + "'><td>" + newTag + "</td><td><button " + click + " class='btn btn-primary'>Delete</button></td></tr>");
-    tags[tags.length] = newTag.toLowerCase();
-    return false;
-}
+    $('#merge_tag').click(function() {
+    });
 
-function deleteTag(tagName) {
-    $("tr[id='" + tagName + "']").hide();
-    tags.splice(tags.indexOf(tagName), 1);
-    return false;
+    $('#add_tag_button').click(function() {
+        var newTag = escapeHTML($('#newTag').val());
+        if (newTag.trim() == "") {
+            alert('empty');
+            return false;
+        }
+        if ($.inArray(newTag.trim().toLowerCase(), tags) != -1) {
+            alert('tag repeated');
+            return false;
+        }
+        $('#newTag').val("");
+
+        var $delete_button = $("<button>", {
+                id: "delete_button",
+                class: "btn btn-primary"
+            })
+            .attr('data-target', newTag).html("Delete")
+            .click(function() {
+                delete_tag($(this).attr('data-target'));
+                return false;
+            });
+        var $checkbox = $("<input>", {
+            type: "checkbox"
+        }).attr('data-target', newTag);
+
+        var $new_row = $("<tr>", {
+                id: newTag
+            })
+            .append($("<td>", {
+                id: "checkbox_td"
+            }).append($checkbox))
+            .append("<td>" + newTag + "</td>")
+            .append($("<td>").append($delete_button));
+
+        $('#tagTable').append($new_row);
+
+        tags[tags.length] = newTag.toLowerCase();
+
+        return false;
+    });
+
+});
+
+function delete_tag(tag_name) {
+    $("tr[id='" + tag_name + "']").hide();
+    tags.splice(tags.indexOf(tag_name), 1);
 }
 
 function submitTag() {
