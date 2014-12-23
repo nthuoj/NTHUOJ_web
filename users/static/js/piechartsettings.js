f8fe4b736be10f108925d2a71d813fe05b586077
henryyang42 <henryyang42@gmail.com>
2014-12-23 22:10:13 +0800

    add license change indent size unify quote for html and js

--- users/static/js/piechartsettings.js
+++ users/static/js/piechartsettings.js
@@ -1,11 +1,35 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 NTHUOJ team
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 $(function() {
     /*
      * insert fake data for demo use
      */
     function labelFormatter(label, series) {
-        return "<div style='font-size:10pt; text-align:center; padding:2px; color:black;'>" + label + "<br/>" + Math.round(series.percent) + "%</div>";
+        return '<div style="font-size:10pt;text-align:center;padding:2px;">' +
+            label + '<br/>' + Math.round(series.percent) + '%</div>';
     }
-    var type = ["WA", "AC", "RE", "TLE", "MLE", "OLE", "Others"];
+    var type = ['WA', 'AC', 'RE', 'TLE', 'MLE', 'OLE', 'Others'];
     var data = [],
         series = 7;
 
@@ -15,8 +39,8 @@ $(function() {
             data: Math.floor(Math.random() * 100) + 50
         }
     }
-    
-    var placeholder = $("#placeholder");
+
+    var placeholder = $('#piechart');
     $.plot(placeholder, data, {
         series: {
             pie: {
@@ -38,13 +62,13 @@ $(function() {
         }
     });
 
-    placeholder.bind("plotclick", function(event, pos, obj) {
+    placeholder.bind('plotclick', function(event, pos, obj) {
 
         if (!obj) {
             return;
         }
         var percent = parseFloat(obj.series.percent).toFixed(2);
 
-        var w = window.open("/status");
+        var w = window.open('/status');
     });
 })
