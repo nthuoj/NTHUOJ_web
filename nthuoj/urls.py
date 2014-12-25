e14b833d2033ea71104955d2476adf6cb5f23ea1
drowsy810301 <lbj39919538@gmail.com>
2014-12-25 23:03:17 +0800

    add inex app in setting.py and modify url.py

--- nthuoj/urls.py
+++ nthuoj/urls.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'index.views.home'),
+    url(r'^broken/', 'index.views.broken'),
+    url(r'^base/', 'index.views.base'),
+    url(r'^get_time/', 'index.views.get_time'),
+    url(r'^status/', 'index.views.status'),
+    url(r'^submit/', 'index.views.submit'),
+    url(r'^group_list/', 'index.views.group_list'),
+    url(r'^team_list/', 'index.views.team_list'),
 )
