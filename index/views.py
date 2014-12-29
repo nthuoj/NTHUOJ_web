6e93da89340fae17d75aa12e227d2ec1b3b0fa1b
drowsy810301 <lbj39919538@gmail.com>
2014-12-29 22:14:08 +0800

    views.py change tab to space

--- index/views.py
+++ index/views.py
@@ -27,31 +27,31 @@ import time
 import datetime
 # Create your views here.
 def home(request):
-	t = time.time()
-    	tstr = datetime.datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
-	return render(request, 'newindex.html', { 'tstr':tstr, 'info1':123, 'info2':123456789, 'Column_content':'Column content', 'contest1':'DS', 'contest2':'junior', 'contest3':'senior', 'contest4':'ABCDEFG', 'contest5':'GGGGGG', 'rtime1':'100 days', 'rtime2':'1 seconds', 'rtime3':'0 seconds', 'utime1':'?????', 'utime2':'@@', 'people':'123' })
+    t = time.time()
+    tstr = datetime.datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
+    return render(request, 'newindex.html', { 'tstr':tstr, 'info1':123, 'info2':123456789, 'Column_content':'Column content', 'contest1':'DS', 'contest2':'junior', 'contest3':'senior', 'contest4':'ABCDEFG', 'contest5':'GGGGGG', 'rtime1':'100 days', 'rtime2':'1 seconds', 'rtime3':'0 seconds', 'utime1':'?????', 'utime2':'@@', 'people':'123' })
 
 def base(request):
-	t = time.time()
-    	tstr = datetime.datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
-	return render(request, 'base.html', { 'tstr':tstr, 'info1':123, 'info2':123456789, 'people':'123' })
+    t = time.time()
+    tstr = datetime.datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
+    return render(request, 'base.html', { 'tstr':tstr, 'info1':123, 'info2':123456789, 'people':'123' })
 
 def broken(request):
-	return render(request, 'brokenpage.html', {'error_message':'error message', 'people':'123'})
+    return render(request, 'brokenpage.html', {'error_message':'error message', 'people':'123'})
 
 def get_time(request):
-	t = time.time()
-    	tstr = datetime.datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
-	return HttpResponse(tstr)
+    t = time.time()
+    tstr = datetime.datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
+    return HttpResponse(tstr)
 
 def submit(request):
-	return render(request, 'submit.html', {'people':'123'})
+    return render(request, 'submit.html', {'people':'123'})
 
 def status(request):
-	return render(request, 'status.html', {'people':'123'})
+    return render(request, 'status.html', {'people':'123'})
 
 def group_list(request):
-	return render(request, 'group_list.html', {'people':'123'})
+    return render(request, 'group_list.html', {'people':'123'})
 
 def team_list(request):
-	return render(request, 'team_list.html', {'people':'123'})
+    return render(request, 'team_list.html', {'people':'123'})
