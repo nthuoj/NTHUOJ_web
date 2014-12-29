516931ba5cd955cdb9890a5a961774cbbf8dcc09
張展榕 <baby19981998@gmail.com>
2014-12-29 15:21:17 +0800

    fix naming problem and title problem

--- contest/static/contest/js/overview.js
+++ contest/static/contest/js/overview.js
@@ -18,14 +18,14 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 var pass = document.getElementsByName("pass");
-var not_pass = document.getElementsByName("not_pass");
-var pass_ratio = document.getElementsByName("pass_ratio");
+var notPass = document.getElementsByName("not_pass");
+var passRatio = document.getElementsByName("pass_ratio");
 var i;
 var j;
 for (i = 0; i < pass.length; i++) {
-    var res = pass_ratio[i].innerHTML.split("/");
-    var pass_ppl = parseInt(res[0]);
-    var all_ppl = parseInt(res[1]);
-    pass[i].style.width = pass_ppl / all_ppl * 100 + '%';
-    not_pass[i].style.width = (1 - pass_ppl / all_ppl) * 100 + '%';
+    var res = passRatio[i].innerHTML.split("/");
+    var passPpl = parseInt(res[0]);
+    var allPpl = parseInt(res[1]);
+    pass[i].style.width = passPpl / allPpl * 100 + '%';
+    notPass[i].style.width = (1 - passPpl / allPpl) * 100 + '%';
 }
--- contest/views.py
+++ contest/views.py
@@ -27,54 +27,54 @@ def index(request):
     problem1 = {'id':1,'name':'First Problem','ppl_pass':2,'ppl_not_pass':4}
     problem2 = {'id':2,'name':'Second Problem','ppl_pass':1,'ppl_not_pass':8}
     problem3 = {'id':3,'name':'third Problem','ppl_pass':5,'ppl_not_pass':1}
-    problem_list = [problem1,problem2,problem3]
+    problemList = [problem1,problem2,problem3]
 
     ppl1 = {'name':'naruto'}
     ppl2 = {'name':'obama'}
     
-    contestant_list = [ppl1,ppl2]
+    contestantList = [ppl1,ppl2]
 
     ppl3 = {'name':'forest'}
     ppl4 = {'name':'gump'}
 
-    coowner_list = [ppl3,ppl4]  
+    coownerList = [ppl3,ppl4]
 
     contest1 = {'id':1,'name':'First contest','start_time':'2014/11/29 10:11:22','end_time':'2014/12/05 02:12:50',
-                'contestant_number':200,'owner':'ma in joe','problem_list':problem_list,'contestant_list':contestant_list,
-                'coowner_list':coowner_list}
+                'contestant_number':200,'owner':'ma in joe','problem_list':problemList,'contestant_list':contestantList,
+                'coowner_list':coownerList}
 
-    contest_list = [contest1]
+    contestList = [contest1]
 
     #contest_list = Contest.objects.order_by('-contest_id')
     #problem_list = Problem.objects.all()
     #contestant_list = {1:'naruto',2:'obama'}
     #coowner_list = {1:'teemo',2:'lux',3:'jinx'}
-    return render(request, 'contest/index.html',{'contest_list':contest_list,'admin':1})
+    return render(request, 'contest/contestArchive.html',{'contest_list':contestList,'admin':1})
 
 def contest(request,contest_id):
 
-    server_time = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    serverTime = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
 
     problem1 = {'id':1,'name':'First Problem','ppl_pass':2,'ppl_not_pass':4}
     problem2 = {'id':2,'name':'Second Problem','ppl_pass':1,'ppl_not_pass':8}
     problem3 = {'id':3,'name':'third Problem','ppl_pass':5,'ppl_not_pass':1}
-    problem_list = [problem1,problem2,problem3]
+    problemList = [problem1,problem2,problem3]
 
     ppl1 = {'name':'naruto','score':['0/3','1/2','1/4'],'solved':'2','panalty':'833'}
     ppl2 = {'name':'obama','score':['0/2','1/5','1/3'],'solved':'2','panalty':'83'}
     
-    contestant_list = [ppl1,ppl2]
+    contestantList = [ppl1,ppl2]
 
     clarification1 = {'id':1,'name':'First Problem','asker':'obama','content':'i have some problem','reply':'shut up'}
 
-    clarification_list = [clarification1]
+    clarificationList = [clarification1]
 
     contest1 = {'name':'First contest','start_time':'2014/12/27 15:30:00','end_time':'2014/12/27 16:00:00',
-                    'contest_contestant':200,'contest_owner':'ma in joe','problem_list':problem_list,'contestant_list':contestant_list
-                    ,'clarification_list':clarification_list}
+                    'contest_contestant':200,'contest_owner':'ma in joe','problem_list':problemList,'contestant_list':contestantList
+                    ,'clarification_list':clarificationList}
 
-    return render(request, 'contest/contest.html',{'contest':contest1,'problem_list':problem_list,
-        'contestant_list':contestant_list,'server_time':server_time})
+    return render(request, 'contest/contest.html',{'contest':contest1,'problem_list':problemList,
+        'contestant_list':contestantList,'server_time':serverTime})
 
 def navbar(request):
     return render(request, 'contest/navbar.html')
