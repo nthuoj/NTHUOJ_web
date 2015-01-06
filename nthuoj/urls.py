5df3632df6471258b3c7b4986f612db288c27d09
張展榕 <baby19981998@gmail.com>
2015-01-06 13:29:55 +0800

    fix url.py conflict

--- nthuoj/urls.py
+++ nthuoj/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+	url(r'^admin/', include(admin.site.urls)),
     url(r'^contest/', include('contest.urls')),
     url(r'^submit/$', 'users.views.submit'),
     url(r'^profile/$', 'users.views.profile'),
