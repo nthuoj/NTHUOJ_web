116ed6ce26bdfc7a502d97d965db1a10fbe29b57
Gordon <geniusgordon@gmail.com>
2015-01-25 17:31:14 +0800

    edit javascript

--- problem/static/problem/js/edit.js
+++ problem/static/problem/js/edit.js
@@ -30,76 +30,9 @@ function switchTab(t) {
 
 var tags = [];
 
-function escapeHTML(str) {
-    return $('<div/>').text(str).html();
-}
-
 $(document).ready(function() {
     $("a[role='tab']").click(function(e) {
         e.preventDefault()
         switchTab(this);
     });
 
-    $('#merge_tag').click(function() {
-        var n = $('input:checked').length;
-        for (var i = 1; i < n; i++) {
-            var d = $('input:checked').eq(i).attr('data-target');
-            delete_tag(d);
-        }
-        return false;
-    });
-
-    $('#add_tag_button').click(function() {
-        var newTag = escapeHTML($('#newTag').val()).trim().toLowerCase();
-        if (newTag == "") {
-            alert('empty');
-            return false;
-        }
-        if ($.inArray(newTag, tags) != -1) {
-            alert('tag repeated');
-            return false;
-        }
-        $('#newTag').val("");
-
-        var $delete_button = $("<button>", {
-                id: "delete_button",
-                class: "btn btn-primary"
-            })
-            .attr('data-target', newTag).html("Delete")
-            .click(function() {
-                delete_tag($(this).attr('data-target'));
-                return false;
-            });
-        var $checkbox = $("<input>", {
-            type: "checkbox"
-        }).attr('data-target', newTag);
-
-        var $new_row = $("<tr>", {
-                id: newTag
-            })
-            .append($("<td>", {
-                id: "checkbox_td"
-            }).append($checkbox))
-            .append("<td>" + newTag + "</td>")
-            .append($("<td>").append($delete_button));
-
-        $('#tagTable').append($new_row);
-
-        tags[tags.length] = newTag.toLowerCase();
-
-        return false;
-    });
-});
-
-function delete_tag(tag_name) {
-    $("tr[id='" + tag_name + "']").hide();
-    tags.splice(tags.indexOf(tag_name), 1);
-}
-
-function submitTag() {
-    var len = tags.length;
-    for (var i = 0; i < len; i++) {
-        $("form").append("<input type='hidden' name='tag" + i + "' value='" + tags[i] + "'>");
-    }
-    return false;
-}
