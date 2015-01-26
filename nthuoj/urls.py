5039c9413f5b4568e3b43dce30cccc000faa0a7a
drowsy810301 <lbj39919538@gmail.com>
2015-01-26 14:08:45 +0800

    fix the clock problem

--- nthuoj/urls.py
+++ nthuoj/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^get_time/', 'index.views.get_time'),
     url(r'^index/', include('index.urls')),
     url(r'^contest/', include('contest.urls')),
     url(r'^users/', include('users.urls')),
