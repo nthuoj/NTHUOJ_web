137899756e8c9f1896bc025f649806803bceb581
張展榕 <baby19981998@gmail.com>
2015-02-04 17:36:49 +0800

    get submission data from database in views.py

--- contest/views.py
+++ contest/views.py
@@ -32,6 +32,7 @@ from contest.models import Clarification
 from contest.forms import ContestForm
 
 from problem.models import Submission
+from problem.models import Problem
 
 def archive(request):
     #to store contest basic info and contestants
@@ -53,21 +54,52 @@ def contest(request,contest_id):
     clarification_list = Clarification.objects.filter(contest = contest)
     contestant_list = Contestant.objects.filter(contest = contest)
     
+    ### get panalty scoreboard ###
+
+    #store contestants' submission dictionary
     submission_list = []
+    #temporary store single contestant submission
     contestant_submission_list = []
     for contestant in contestant_list:
         for problem in contest.problem.all():
-            submission = Submission.objects.filter(problem = problem , submit_time = contest.end_time , user = contestant.user)
+            submission = Submission.objects.filter(problem = problem , submit_time__lte = contest.end_time,
+                submit_time__gte = contest.start_time , user = contestant.user)
             contestant_submission_list.append({'submission':submission})
         
         submission_list.append({'contestant_submission_list':contestant_submission_list})
+        contestant_submission_list = []
     
-    #for contestant_submission_list in submission_list:
-        #for submission in contestant_submission_list:
-            #if submission 
+    #store all contestants' data
+    scoreboard = []
+    #store single contestant score
+    contestant_scoreboard = []
+    #store single problem status (pass or not)
+    status = {}
+    #store single problem submit times
+    times = {}
+
+    #get single contestant
+    for contestant_submission_list in submission_list:
+        times = {}
+        status = {}
+        #count panalty
+        panalty = 0
+
+        #get single contestant's submissions
+        for submissions in contestant_submission_list['contestant_submission_list']:
+            #get single contestant's single submission
+            for submission in submissions['submission']:
+                times[submission.problem.pname.encode('utf8')] = times.get(submission.problem.pname.encode('utf8'),0) + 1
+                if submission.status == Submission.ACCEPTED:
+                    status[submission.problem.id] = 1
+
+            contestant_scoreboard.append({'status':status,'times':times})
+        scoreboard.append({'contestant_scoreboard':contestant_scoreboard})
+        contestant_scoreboard = []
+
 
     return render(request, 'contest/contest.html',{'contest':contest,'clarification_list':clarification_list,
-        'contestant_list':contestant_list,'contestant_submission_list':contestant_submission_list,'server_time':serverTime},
+        'contestant_list':contestant_list,'scoreboard':scoreboard,'server_time':serverTime},
         context_instance = RequestContext(request, processors = [custom_proc]))
 
 def new(request):
