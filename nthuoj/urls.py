6185daa5590a81762c07b53bde11cb172ecd1676
drowsy810301 <lbj39919538@gmail.com>
2015-02-05 22:06:00 +0800

    use url tag to link ver2

--- nthuoj/urls.py
+++ nthuoj/urls.py
@@ -4,13 +4,14 @@ from django.contrib import admin
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^problem/', include('problem.urls')),
+    url(r'^broken/', 'index.views.broken'),
     url(r'^get_time/', 'index.views.get_time'),
     url(r'^index/', include('index.urls', namespace='index')),
+    url(r'^problem/', include('problem.urls', namespace='problem')),
     url(r'^contest/', include('contest.urls', namespace='contest')),
     url(r'^users/', include('users.urls', namespace='users')),
     url(r'^team/', include('team.urls', namespace='team')),
     url(r'^group/', include('group.urls', namespace='group')),
     url(r'^status/', include('status.urls', namespace='status')),
-    url(r'^broken/', 'index.views.broken'),
+    
 )
