6e5c3535aa7d8798eaa529e98ebcfaf20c9c3a75
drowsy810301 <lbj39919538@gmail.com>
2015-02-05 13:34:38 +0800

    method get_ownership

---
+++ general_tools/ownership.py
@@ -0,0 +1,36 @@
+'''
+The MIT License (MIT)
+
+Copyright (c) 2014 NTHUOJ team
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOsT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+from django.db import models
+from users.models import User
+
+def has_cownership(curr_user, curr_contest):
+    ownership = (curr_user.username == curr_contest.owner)
+    for coowner in curr_contest.coowner:
+        if curr_user == coowner
+            ownership = True
+    return ownership
+
+def has_pownership(curr_user, curr_problem):
+    ownership = (curr_user.username == curr_problem.owner)
+    return ownership
