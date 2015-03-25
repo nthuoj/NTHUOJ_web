131d55a60db5380c32843946ab3ea36d84180450
Gordon <geniusgordon@gmail.com>
2015-03-25 11:01:41 +0800

    add get problem list

--- problem/problem_info.py
+++ problem/problem_info.py
@@ -1,6 +1,7 @@
 import os.path
 from utils import config_info
 from problem.models import Problem, Testcase
+from django.db.models import Q
 
 SPECIAL_PATH = config_info.get_config('path', 'special_judge_path')
 PARTIAL_PATH = config_info.get_config('path', 'partial_judge_path')
@@ -8,6 +9,15 @@ PARTIAL_PATH = config_info.get_config('path', 'partial_judge_path')
 def get_problem(problem):
     problem.testcase = Testcase.objects.filter(problem=problem)
     return problem
+
+def get_problem_list(user):
+    if user.is_anonymous():
+        return Problem.objects.all(visible=True)
+    else:
+        if user.is_admin:
+            return Problem.objects.all()
+        else:
+            return Problem.objects.filter(Q(visible=True) | Q(owner=request.user))
 
 def has_special_judge_code(problem):
     return os.path.isfile("%s%d.c" % (SPECIAL_PATH, problem.pk))
