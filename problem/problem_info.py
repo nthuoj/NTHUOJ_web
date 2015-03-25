25acc2493ffebad8a9c54829d5ae3e03c5052663
Gordon <geniusgordon@gmail.com>
2015-03-25 10:19:47 +0800

    store path to a constant

--- problem/problem_info.py
+++ problem/problem_info.py
@@ -2,15 +2,16 @@ import os.path
 from utils import config_info
 from problem.models import Problem, Testcase
 
+SPECIAL_PATH = config_info.get_config('path', 'special_judge_path')
+PARTIAL_PATH = config_info.get_config('path', 'partial_judge_path')
+
 def get_problem(problem):
     problem.testcase = Testcase.objects.filter(problem=problem)
     return problem
 
 def has_special_judge_code(problem):
-    speJudge_path = config_info.get_config('path', 'special_judge_path')
-    return os.path.isfile("%s%d.c" % (speJudge_path, problem.pk))
+    return os.path.isfile("%s%d.c" % (SPECIAL_PATH, problem.pk))
 
 def has_partial_judge_code(problem):
-    partial_path = config_info.get_config('path', 'partial_judge_path')
-    return os.path.isfile("%s%d.c" % (partial_path, problem.pk))
+    return os.path.isfile("%s%d.c" % (PARTIAL_PATH, problem.pk))
 
