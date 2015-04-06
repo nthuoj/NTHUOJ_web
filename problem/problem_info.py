3564c51faa069e7ad1d5cf25438c56c7550e60aa
Gordon <geniusgordon@gmail.com>
2015-04-06 17:31:23 +0800

    add order_by id

--- problem/problem_info.py
+++ problem/problem_info.py
@@ -11,12 +11,12 @@ def get_testcase(problem):
 
 def get_problem_list(user):
     if user.is_anonymous():
-        return Problem.objects.filter(visible=True)
+        return Problem.objects.filter(visible=True).order_by('id')
     else:
         if user.is_admin:
-            return Problem.objects.all()
+            return Problem.objects.all().order_by('id')
         else:
-            return Problem.objects.filter(Q(visible=True) | Q(owner=user))
+            return Problem.objects.filter(Q(visible=True) | Q(owner=user)).order_by('id')
 
 def has_special_judge_code(problem):
     if problem.judge_language == problem.C:
