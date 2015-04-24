c155c36256b23e35595f4a142581d9f51014b17a
Gordon <geniusgordon@gmail.com>
2015-04-24 20:13:16 +0800

    problem edit

--- problem/views.py
+++ problem/views.py
@@ -70,9 +70,9 @@ def detail(request, pid):
     tag_form = TagForm()
     try:
         problem = Problem.objects.get(pk=pid)
-        user = validate_user(reuqest.user)
-        if not has_problem_auth(user, problem)
-            raise PermissionDeny()
+        user = validate_user(request.user)
+        if not has_problem_auth(user, problem):
+            raise PermissionDenied()
     except Problem.DoesNotExist:
         logger.warning('problem %s not found' % (pid))
         raise Http404('problem %s does not exist' % (pid))
