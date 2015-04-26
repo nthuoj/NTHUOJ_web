ed0a1526caf8dc501d325fa3dd79be36ed741823
henryyang42 <henryyang42@gmail.com>
2015-04-26 23:02:43 +0800

    split compiler option into single line

--- users/forms.py
+++ users/forms.py
@@ -42,7 +42,7 @@ class CodeSubmitForm(forms.Form):
     pid = forms.CharField(label='Problem ID')
     language = forms.ChoiceField(choices=LANGUAGE_CHOICE, initial=Submission.CPP,
                                  widget=forms.RadioSelect(),
-                                 help_text="Backend: %s<br>gcc: %s, g++: %s"
+                                 help_text="Backend: %s<br>gcc: %s<br>g++: %s"
                                  % (BACKEND_VERSION, GCC_VERSION, GPP_VERSION))
     code = forms.CharField(max_length=10000,
                            widget=forms.Textarea(attrs={'id': 'code_editor'}))
