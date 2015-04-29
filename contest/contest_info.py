d2126688bc671d9c617e76eb328d00616cd1f5f1
ChangRon <changrontw@gmail.com>
2015-04-29 22:56:19 +0800

    fix scoreboard chinese filename

--- contest/contest_info.py
+++ contest/contest_info.py
@@ -145,7 +145,7 @@ def get_scoreboard_csv(contest_id, scoreboard_type):
     scoreboard = get_scoreboard(contest)
     
     response = HttpResponse(content_type='text/csv')
-    filename = str(contest.cname) + '-scoreboard-' + str(scoreboard_type)
+    filename = contest.cname.encode('utf-8') + '-scoreboard-' + str(scoreboard_type)
     response['Content-Disposition'] = 'attachment; filename=' + filename
 
     #init
