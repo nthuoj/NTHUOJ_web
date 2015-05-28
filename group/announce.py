b750a689255aa2e7afeb29994f8da0a37183db14
Casmo <seraphim.aria@gmail.com>
2015-05-28 20:35:09 +0800

    add logger for delete announce

--- group/announce.py
+++ group/announce.py
@@ -60,6 +60,7 @@ def delete_announce(request, announce_id, group_id):
 
     if has_group_ownership(request.user, group) or has_group_coownership(request.user, group):  
         get_announce(announce_id).delete()
+        logger.info('Announce: User %s delete Announce %s!' % (request.user.username, announce.id))
         return HttpResponseRedirect(reverse('group:detail', kwargs={'group_id': group_id}))
     else:
         raise PermissionDenied
