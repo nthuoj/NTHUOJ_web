1f12a7f293fa1990e6ffc19e38341ef8f7b5797d
Casmo <seraphim.aria@gmail.com>
2015-05-31 23:03:44 +0800

    remove default value for annoucne title

--- group/models.py
+++ group/models.py
@@ -30,7 +30,7 @@ from datetime import date
 
 class Announce(models.Model):
 
-    title = models.CharField(max_length=100, default='Title')
+    title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
 
     def __unicode__(self):
