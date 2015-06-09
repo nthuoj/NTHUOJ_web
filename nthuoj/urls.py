e1e67a9ac0e360d44db1675190c5ba9d71bf12dc
henryyang42 <henryyang42@gmail.com>
2015-06-09 15:07:46 +0800

    override url

--- nthuoj/urls.py
+++ nthuoj/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from ckeditor.views import upload, browse
 import autocomplete_light
-
 # OP autodiscover
 autocomplete_light.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^ckeditor/upload/', upload, name='ckeditor_upload'),
+    url(r'^ckeditor/browse/', browse, name='ckeditor_browse'),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     url(r'^autocomplete/', include('autocomplete_light.urls')),
-    url(r'^ckeditor/', include('ckeditor.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^get_time/', 'index.views.get_time'),
     url(r'^', include('index.urls', namespace='index')),
@@ -19,6 +20,7 @@ urlpatterns = patterns('',
     url(r'^team/', include('team.urls', namespace='team')),
     url(r'^group/', include('group.urls', namespace='group')),
     url(r'^status/', include('status.urls', namespace='status')),
+
 )
 handler400 = 'index.views.custom_400'
 handler403 = 'index.views.custom_403'
