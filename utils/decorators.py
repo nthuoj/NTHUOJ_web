d4fd2b489a6780a0210251325ebf1a17b9457eee
henryyang42 <henryyang42@gmail.com>
2015-07-04 12:57:26 +0800

    add missing import

--- utils/decorators.py
+++ utils/decorators.py
@@ -21,8 +21,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from utils.user_info import validate_user
+
 
 def subjudge_auth_required(view):
     """A decorator to ensure user has judge auth."""
